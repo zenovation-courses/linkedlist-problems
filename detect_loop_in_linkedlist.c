d6244c48f53b6ecfddd681329ea32983274d47de
ZENOVATION online courses <99176157+zenovation-courses@users.noreply.github.com>
2026-04-21 13:46:39 +0530

    Create detect_loop_in_linkedlist.c

---
+++ detect_loop_in_linkedlist.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+
+void printLList(struct Node* head){
+    while(head){
+        printf("%d  ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void addNode(struct Node** newHead, int num){
+    
+    struct Node* temp = 
+        (struct Node*)malloc(sizeof(struct Node));
+    temp->data = num;
+    temp->next = *newHead;
+    *newHead = temp;
+}
+
+void detectCycle(struct Node* head){
+    struct Node* slow = head, *fast = head;
+    
+    while ( fast && fast->next ){
+        fast = fast->next->next;
+        slow = slow->next;
+        
+        if (fast == slow){
+            printf("Cycle found\n");
+            return;
+        }
+    }
+    printf("No cycle found\n");
+    return;
+}
+
+int main(){
+    
+    struct Node* head = NULL;
+    
+    addNode(&head, 23);
+    struct Node* temp = head;
+    addNode(&head, 43);
+    addNode(&head, 7);
+    temp->next = head;
+    
+    addNode(&head, 2);
+    addNode(&head, 11);
+    
+    detectCycle(head);
+    
+
+}
+    
+    
+
+
